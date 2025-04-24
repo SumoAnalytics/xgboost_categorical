c3c469c15654a8673c5352004ef14eb828713f31
vidarsumo <vidar@sumoanalytics.es>
2025-04-24 16:03:49 +0200

    upd

--- xgb_cat.R
+++ xgb_cat.R
@@ -3,8 +3,8 @@ library(tidymodels)
 library(tidyverse)
 
 mtcars_cat <- mtcars %>% 
-  mutate(hp = paste0("cat_", hp)) %>% 
-  as_tibble()
+  mutate(hp = as.factor(paste0("cat_", hp))) %>% 
+  as.data.frame()
 
 
 xgb_cat <- boost_tree(mode = "regression") %>% 
@@ -13,3 +13,5 @@ xgb_cat <- boost_tree(mode = "regression") %>%
 
 
 vip::vip(xgb_cat)
+
+
